c8a9e62ee00b2bc13fe28ca52737c4b52a439807
Raoul Comninos <revcomninos@gmail.com>
2023-08-11 15:52:48 +0200

    updates

---
+++ 21-of-the-Finest-Emacs-Themes-My-Current-Favorite.el
@@ -0,0 +1,28 @@
+;;; 21 of the Finest Emacs Themes + My Current Favorite
+
+;; Copyright (C) 2023  
+;; Author:  Raoul Comninos
+
+;; Keywords: themes
+
+;; spacemacs-dark
+;; zenburn
+;; doom-one
+;; monokai
+;; dracula-theme
+;; solarized-light
+;; solarized-dark
+;; color-theme-sanityinc-tomorrow-blue
+;; gruvbox-dark-medium
+;; material-light
+;; moe-light
+;; cyberpunk-theme
+;; ample-theme
+;; apropospriate-light
+;; alect-black
+;; anti-zenburn-theme
+;; tao-theme-yin
+;; minimal-light
+;; modus-vivendi
+;; inkpot
+;; plain-theme
