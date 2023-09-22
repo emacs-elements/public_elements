c908a59540efd55449a6c0c11bc2ee5389685e63
Raoul Comninos <revcomninos@gmail.com>
2023-09-22 15:16:04 +0200

    updates

---
+++ hotkey.el
@@ -0,0 +1,12 @@
+(defun jump-out-of-pair ()
+	(interactive)
+	(let ((found (search-forward-regexp "[])}\"'`*=]" nil t)))
+		(when found
+			(cond ((or (looking-back "\\*\\*" 2)
+		 (looking-back "``" 2)
+		 (looking-back "''" 2)
+		 (looking-back "==" 2))
+			 (forward-char))
+			(t (forward-char 0))))))
+
+(global-set-key (kbd "M-1") 'jump-out-of-pair)
