81512a322b1a3ddb3f8da2775f70b41b975ed151
Raoul Comninos <revcomninos@gmail.com>
2023-10-13 17:13:39 +0200

    updates

---
+++ transfer-google-cal.el
@@ -0,0 +1,10 @@
+(require 'icalendar)
+
+(setq diary-file "~/.emacs.d/diary")
+(setq calendar-mark-diary-entries-flag t)
+(add-to-list 'auto-mode-alist '("\\diary\\'" . diary-mode))
+(setq diary-comment-start ";;")
+(setq diary-comment-end "")
+(setq org-agenda-include-diary t)
+(add-hook 'diary-mode-hook 'real-auto-save-mode)
+(add-hook 'diary-mode-hook #'abbrev-mode)
