84c18176020d6364430a6a9c8f70ef24cba201ae
Raoul Comninos <revcomninos@gmail.com>
2023-11-27 11:23:38 +0200

    updates

---
+++ selection-highlight-mode.el
@@ -0,0 +1,14 @@
+;; Selection Highlight Mode
+;; URL: https://github.com/balloneij/selection-highlight-mode
+;; Inspired by the Selection Highlight setting found in VS Code
+;; It highlights matches across all active windows
+;; If an action is thereafter applied to the selection
+;; it will operate only on the one.
+;; It is a visual aid and will not work for multiple edits.
+;; It is not available on Melpa.
+
+(require 'selection-highlight-mode)
+(selection-highlight-mode +1)
+
+;; Goodbye
+;; Goodbye
