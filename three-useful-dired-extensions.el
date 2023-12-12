3a85ce7758db7468dc8cd2cb4523d7eafc4e7f83
Raoul Comninos <revcomninos@gmail.com>
2023-12-12 06:46:09 +0200

    updates

--- three-useful-dired-extensions.el
+++ three-useful-dired-extensions.el
@@ -1,10 +1,30 @@
-;; Three useful Dired Extensions
+S;; Three useful Dired Extensions
 
 ;; The default behaviour sorts by "name" and "date"
 
 (require 'dired-quick-sort)
 
 (dired-quick-sort-setup)
+
+
+
+
+1. default: This option uses the default file sorting method of the underlying system. On Unix-like systems, this typically corresponds to mtime, sorting by modification time.
+
+2. atime: This option sorts by access time, which is the last time the file was read or its metadata was accessed. This can be useful if you're interested in identifying files that haven't been accessed in a while.
+
+3. access: Similar to atime, this option also sorts by access time. However, it uses a more precise timestamp that includes both date and time information.
+
+4. use: This option sorts by use time, which is the last time the file was modified or accessed in a way that changed its data content. This can be useful for identifying recently edited files.
+
+5. ctime: This option sorts by creation time, which is the time the file was created. This can be helpful for finding chronologically older files.
+
+6. Status: This option doesn't directly sort by time, but instead groups files based on their modification status. It categorizes files as "modified," "unchanged," "missing," and "deleted." This provides a quick overview of the changes made within the directory.
+
+
+
+
+
 
 ;; -------------------------------------------------------------------block-ends-here
 ;; Install dired-hacks-utils and dired-narrow
